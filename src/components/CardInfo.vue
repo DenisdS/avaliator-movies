c39e1c769d0a86278e8fb7414d481ed78f570828
DenisdS <denis_santos1989@hotmail.com>
2019-12-12 17:26:04 -0300

    Test infos by component using props

--- src/components/CardInfo.vue
+++ src/components/CardInfo.vue
@@ -5,14 +5,28 @@ export default {
     card: {
       type: Object,
       required: true,
-      default: () => ({})
     }
   }
 }
 </script>
 
 <template>
-  <div>
-    CardInfo
+  <div class="card-info">
+    <img class="card-info__poster" :src="card.poster_url" />
+    <div class="card-info__title">
+      {{ card.title }}
+    </div>
+    <div class="card-info__date">
+      {{ card.release_date }}
+    </div>
+    <div class="card-info__genre">
+      {{ card.genre_id }}
+    </div>
+    <div class="card-info__populary">
+      {{ card.populary }}
+    </div>
+    <div class="card-info__overview">
+      {{ card.overview }}
+    </div>
   </div>
 </template>
---
+++ tests/unit/CardInfo.spec.js
@@ -0,0 +1,58 @@
+import { shallowMount } from '@vue/test-utils'
+import CardInfo from '@/components/CardInfo'
+import card from './fixtures/card'
+
+describe('CardInfo', () => {
+  let props
+
+  const build = () => {
+    const wrapper = shallowMount(CardInfo, {
+      propsData: props,
+    })
+    return {
+      wrapper,
+      title: () => wrapper.find('.card-info__title'),
+      poster_path: () => wrapper.find('.card-info__poster'),
+      release_date: () => wrapper.find('.card-info__date'),
+      genre_id: () => wrapper.find('.card-info__genre'),
+      populary: () => wrapper.find('.card-info__populary'),
+      overview: () => wrapper.find('.card-info__overview'),
+    }
+  }
+
+  beforeEach(() => {
+    props = {
+      card
+    }
+  })
+
+  it('renders the component CardInfo', () => {
+
+    const { wrapper } = build()
+    
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('renders main components via props', () => {
+    
+    const { title, poster_path, release_date, genre_id, populary, overview } = build()
+    
+    expect(title().exists()).toBe(true)
+    expect(title().text()).toBe(props.card.title)
+
+    expect(poster_path().exists()).toBe(true)
+    expect(poster_path().attributes().src).toBe(props.card.poster_url)
+
+    expect(release_date().exists()).toBe(true)
+    expect(release_date().text()).toBe(props.card.release_date)
+
+    expect(genre_id().exists()).toBe(true)
+    expect(genre_id().text()).toBe(props.card.genre_id)
+
+    expect(populary().exists()).toBe(true)
+    expect(populary().text()).toBe(props.card.populary)
+
+    expect(overview().exists()).toBe(true)
+    expect(overview().text()).toBe(props.card.overview)
+  })
+})
